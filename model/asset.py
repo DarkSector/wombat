4502aa80fac8fb35ffcc39ce6a29c2bd201e3f41
Pronoy Chopra <pronoyc@gmail.com>
2011-07-01 15:30:11 +0530

    minor modification

--- model/asset.py
+++ model/asset.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flaskext.sqlalchemy import SQLAlchemy
-
+from wombatdb import db
 class Asset(db.Model):
-	__tablename__ = 'assets'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.Text, default=u'Unnamed Asset')
-	used_by = db.Column(db.Integer, db.ForeignKey('collections.id'))
 
-def __init__(self,name,tags[]):
-	self.name = name
-	self.tags = tags
-	
+    def create_db():
+        __tablename__ = 'assets'
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.Text, default=u'Unnamed Asset')
+        used_by = db.Column(db.Integer, db.ForeignKey('collections.id'))
+
+    def __init__(self,name,tags):
+        self.name = name
+        self.tags = tags
