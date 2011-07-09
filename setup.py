7076f1825af2e9a4f094aba5e8a230965d1d5c93
Pronoy Chopra <pronoyc@gmail.com>
2011-07-09 23:49:11 +0530

    setup was updated to setup repository

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #setup the application the starting point to everything
 import wombatdb
-
-
+from wombat_config.config_file import REPO_URL
+from backend.svnfunctions import SVNfunctions
 #Create the database
-wombatdb.init_db()
 
+svn = SVNfunctions()
+
+wombatdb.init_db()
+svn.checkout_repo(REPO_URL)
