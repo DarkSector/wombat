94324eae9b3003d89ff0939878112ec1e884ce70
Pronoy Chopra <pronoyc@gmail.com>
2011-07-10 00:36:28 +0530

    updated functions for svn to include ls and url fetching

--- backend/svnfunctions.py
+++ backend/svnfunctions.py
@@ -5,3 +5,17 @@ class SVNfunctions:
     def checkout_repo(self,link):
         client = pysvn.Client()
         client.checkout(link,'./repository')
+
+    def update_copy(self):
+        client = pysvn.Client()
+        client.update('./repository')
+
+    def get_url(self):
+        client = pysvn.Client()
+        entry = client.info('.')
+        return entry.url
+
+    def get_dir_content(self,updated_link):
+        client = pysvn.Client()
+        entry_list = client.ls('.')
+        return entry_list
