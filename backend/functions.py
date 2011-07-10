5aee7a2ac9cbe76ed190c0ed7aa04980814c4e77
Pronoy Chopra <pronoyc@gmail.com>
2011-07-10 21:16:42 +0530

    fixed bug in get_info() function, attribute error

--- backend/functions.py
+++ backend/functions.py
@@ -1,6 +1,5 @@
 #important functions
-
-import os.path
+import os
 
 img_inline = ['.gif', '.jpg', '.png']
 image_exts = ['.bmp', '.gif', '.ico', '.jpg', '.png', '.psd',
@@ -13,26 +12,54 @@ sound_exts = ['.mid', '.mp3', '.ogg', '.wav']
 text_exts = ['.asm', '.bat', '.cfg', '.cg', '.conf', '.glsl', '.hlsl','.htm',
         '.html', '.ini', '.material', '.txt', '.url', '.xml']
 
-def getType(name):
-    """
-    returns the type of file. Example return image if the extension of the file is anything
-    in the image_exts like .jpg .png .gif etc.
-    """
-    base, ext = os.path.splitext(name)
-    ext = ext.lower()
 
-    if ext in image_exts:
-        type = "image"
-    elif ext in model_exts:
-        type = "model"
-    elif ext in sound_exts:
-        type = "sound"
-    elif ext in text_exts:
-        type = "text"
-    else:
-        type = "other"
 
-    return type
+class Base:
+    def getType(self,name):
+        """
+        returns the type of file. Example return image if the extension of the file is anything
+        in the image_exts like .jpg .png .gif etc.
+        """
+        base, ext = os.path.splitext(name)
+        ext = ext.lower()
+
+        if ext in image_exts:
+            type = "image"
+        elif ext in model_exts:
+            type = "model"
+        elif ext in sound_exts:
+            type = "sound"
+        elif ext in text_exts:
+            type = "text"
+        else:
+            type = "other"
+
+        return type
+
+    def get_info(self,link):
+		import os
+		fileList = []
+		fileSize = 0
+		folderCount = 0
+		rootdir = link
+		
+		for root, subFolders, files in os.walk(rootdir):
+		    folderCount += len(subFolders)
+		    for file in files:
+		        f = os.path.join(root,file)
+		        fileSize = fileSize + os.path.getsize(f)
+		        #print(f)
+		        fileList.append(f)
+		
+		
+		
+		fileSize2 = (fileSize / (1024 * 1024) )
+		print("Total Size is {0} bytes".format(fileSize2))
+		print fileList
+		print("Total Files ", len(fileList))
+		print("Total Folders ", folderCount)
+
+
 
 
 #def check_field(data,table):
