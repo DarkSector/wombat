db130d008664486ab8ba5a1198d926983f5f1da2
Pronoy Chopra <pronoyc@gmail.com>
2011-07-10 21:39:31 +0530

    corrected import error

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #setup the application the starting point to everything
 import wombatdb
-from wombat_config.config_file import REPO_URL
+from wombat_config.config_file import REPO_URL,LOCAL_REPO
 from backend.svnfunctions import SVNfunctions
 #Create the database
 
 svn = SVNfunctions()
 
 wombatdb.init_db()
-svn.checkout_repo(REPO_URL)
+svn.checkout_repo(REPO_URL,LOCAL_REPO)
