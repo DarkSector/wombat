f12df10b3f11032606bae800802ad708e1a95c3f
Pronoy Chopra <pronoyc@gmail.com>
2011-07-10 21:17:29 +0530

    added absolute path to the config_file

--- wombat_config/config_file.py
+++ wombat_config/config_file.py
@@ -1,5 +1,7 @@
 #wombat 2011
 # configuration
+import os
+wd = os.getcwd()
 DB_URI = 'sqlite:///wombatDatabase.db'
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -7,3 +9,4 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 DATABASE = 'wombatDatabase.db'
 REPO_URL = 'http://localhost/svn/dummy'
+LOCAL_REPO = wd +'/repository'
