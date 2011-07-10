3d0a3227c6e14a842103bd70941c0cdb76f758fd
Pronoy Chopra <pronoyc@gmail.com>
2011-07-10 21:38:11 +0530

    removed argument error

--- backend/svnfunctions.py
+++ backend/svnfunctions.py
@@ -2,9 +2,9 @@ import pysvn
 
 
 class SVNfunctions:
-    def checkout_repo(self,link):
+    def checkout_repo(self,link,local_link):
         client = pysvn.Client()
-        client.checkout(link,'./repository')
+        client.checkout(link,local_link)
 
     def update_copy(self):
         client = pysvn.Client()
@@ -12,10 +12,15 @@ class SVNfunctions:
 
     def get_url(self):
         client = pysvn.Client()
-        entry = client.info('.')
+        entry = client.info('./repository')
         return entry.url
 
     def get_dir_content(self,updated_link):
         client = pysvn.Client()
         entry_list = client.ls('.')
         return entry_list
+
+    def get_revision_no(self):
+        client = pysvn.Client()
+        rev_no = client.info('./repository').revision.number
+        return rev_no
