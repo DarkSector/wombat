b9fc1410c016f2a2274c6ca8b0270707e84e1432
Pronoy Chopra <pronoyc@gmail.com>
2011-07-16 22:48:19 +0530

    added the rotation and screen capture functionality

--- 3Dtests/Ogre3D/TutorialApplication.h
+++ 3Dtests/Ogre3D/TutorialApplication.h
@@ -8,17 +8,40 @@ Filename:    TutorialApplication.h
 #define __TutorialApplication_h_
 
 #include "BaseApplication.h"
+#include "GL/gl.h"
+#include "stdio.h"
 
 class TutorialApplication : public BaseApplication
 {
 public:
     TutorialApplication(void);
     virtual ~TutorialApplication(void);
-
+    
 protected:
     virtual void createScene(void);
     virtual void createCamera(void);
     virtual void createViewport(void);
+    
+    bool mouseMoved( const OIS::MouseEvent &arg ) {};
+    bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id ) {};
+    bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id ) {};
+    
+    
+    //virtual void writeContentsToFile(const String & filename);
+    bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+	bool frameEnded(const Ogre::FrameEvent& evt)
+	{
+		//updateStats();
+		return true; 
+	};
+	int imagecount;
+	
+	Ogre::Entity *ent;
+	
+	float currentAngle;
+	float accumulatedTime;
+	
+	Ogre::SceneNode *node;
 };
 
 #endif // #ifndef __TutorialApplication_h_
