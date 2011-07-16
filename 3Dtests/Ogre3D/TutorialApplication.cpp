2efd9dd5061d1ba1b51d8b14d17b1d894368032a
Pronoy Chopra <pronoyc@gmail.com>
2011-07-16 22:47:48 +0530

    added the rotation and screen capture functionality

--- 3Dtests/Ogre3D/TutorialApplication.cpp
+++ 3Dtests/Ogre3D/TutorialApplication.cpp
@@ -8,6 +8,9 @@ Filename:    TutorialApplication.cpp
 //-------------------------------------------------------------------------------------
 TutorialApplication::TutorialApplication(void)
 {
+	currentAngle = 10;
+	accumulatedTime = 0;
+	imagecount = 0;
 }
 //-------------------------------------------------------------------------------------
 TutorialApplication::~TutorialApplication(void)
@@ -28,6 +31,8 @@ void TutorialApplication::createCamera(void)
 
 void TutorialApplication::createViewport(void)
 {
+	
+	
 
 }
 
@@ -35,26 +40,79 @@ void TutorialApplication::createViewport(void)
 void TutorialApplication::createScene(void)
 {
     //create primary entity
-    Ogre::Entity* ent = mSceneMgr->createEntity("Ogrehead","ogrehead.mesh");
+    ent = mSceneMgr->createEntity("Ogrehead","ogrehead.mesh");
     
     //this entity is for testing only
     //Ogre::Entity* ent2 = mSceneMgr->createEntity("Ogrehead2","ogrehead.mesh");
     
     //cretae node to which .mesh entity will be attached
-    Ogre::SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode("Node1");
+    node = mSceneMgr->getRootSceneNode()->createChildSceneNode("Node1");
     node->setPosition(10,0,0);
     node->attachObject(ent);
+    
 
-    Ogre::SceneNode* camnode = node->createChildSceneNode("Node2",Ogre::Vector3(80,0,0));
+    //Ogre::SceneNode* camnode = node->createChildSceneNode("Node2",Ogre::Vector3(80,0,0));
     //camnode->attachObject(ent2);
     //camnode->attachObject(cam1);
-    camnode->attachObject(mCamera);
+    //camnode->attachObject(mCamera);
+    
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(1,1,1));
+    
     Ogre::Light* light = mSceneMgr->createLight("mainlight");
     light->setDiffuseColour(Ogre::ColourValue::White);
     light->setPosition(10,100,0);
-    mCamera->setAutoTracking(true, node);
+    //mCamera->setAutoTracking(true, node);
 
 }
+
+
+bool TutorialApplication::frameRenderingQueued(const Ogre::FrameEvent& evt)
+{
+	char filename[100];
+	if(mWindow->isClosed())	return false;
+
+    if(mShutDown)
+        return false;
+
+	float time;
+
+	time = evt.timeSinceLastFrame;
+	
+	accumulatedTime += time;
+	
+	
+	//printf("Frame time = %f\n", time);
+	
+	//if (accumulatedTime > 0.5) {
+		//accumulatedTime = 0;
+		
+		//printf("Inside yaw code\n");
+		
+		if (imagecount == 35)
+			return false;
+		//currentAngle == 0;
+		
+	    node->yaw(Ogre::Degree(currentAngle));
+	    //currentAngle += 10.0;
+	    
+	    glPixelStorei(GL_PACK_ALIGNMENT,1);
+	    
+	    sprintf(filename,"file-%d.png", imagecount);
+	    imagecount++;
+	    mWindow->writeContentsToFile(filename);
+	    printf("%s\n",filename);
+	//}
+	
+	//Need to capture/update each device
+    mKeyboard->capture();
+    mMouse->capture();
+        
+	//printf("Time since last frame = %f", time);
+	
+	
+	return true;
+}
+
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
