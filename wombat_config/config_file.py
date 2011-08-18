fdefe698520d4ef5e02430e1497be2354a092fdd
Pronoy Chopra <pronoyc@gmail.com>
2011-08-18 15:22:55 +0530

    edited config for mail

--- wombat_config/config_file.py
+++ wombat_config/config_file.py
@@ -29,3 +29,14 @@ FIRST_VIEW= False
 
 #construction flag
 CONSTRUCTION = True
+
+#---------------------------Flask-Mail config-----------------------------------
+
+MAIL_SERVER = 'Localhost'
+MAIL_PORT = 25
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+#MAIL_DEBUG = app.debug
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+DEFAULT_MAIL_SENDER = None
